a375cb0c599cae688b1bc43e62f35848cad615d0
chihiro <ex14.mil30@gmail.com>
2026-01-12 22:21:50 +0900

    管理画面　リストタイトルとアイテムリスト　作成完了

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import List, Item
 
-# Register your models here.
+
+class ItemInline(admin.TabularInline):
+    model = Item
+    extra = 1
+
+
+@admin.register(List)
+class ListAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "updated_at")
+    ordering = ("-updated_at",)
+    search_fields = ("title",)
+    inlines = [ItemInline]
