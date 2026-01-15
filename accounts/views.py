a099b40ca6687ea91931a76c38193ee73876bed7
chihiro <ex14.mil30@gmail.com>
2026-01-15 15:21:09 +0900

    アカウント登録画面　views修正、html完成

--- accounts/views.py
+++ accounts/views.py
@@ -20,32 +20,37 @@ def register_view(request):
         password1 = request.POST.get("password1", "")
         password2 = request.POST.get("password2", "")
 
-        errors = []
+        errors = {}
 
-        if not (1 <= len(username) <= 20):
-            errors.append("ユーザー名は１文字～２０文字で入力してください")
         if not username:
-            errors.append("ユーザー名を入力してください")
-        if User.objects.filter(username=username).exists():
-            errors.append("このユーザーは既に使われています")
+            errors.setdefault("username", []).append("ユーザー名を入力してください")
+        else:
+            if not (1 <= len(username) <= 20):
+                errors.setdefault("username", []).append("ユーザー名は１文字～２０文字で入力してください")
+            if User.objects.filter(username=username).exists():
+                errors.setdefault("username", []).append("このユーザーは既に使われています")
 
         if not email:
-            errors.append("メールアドレスを入力してください")
-        if User.objects.filter(email=email).exists():
-            errors.append("このメールアドレスは既に使われています")
+            errors.setdefault("email", []).append("メールアドレスを入力してください")
+        else:
+            if User.objects.filter(email=email).exists():
+                errors.setdefault("email", []).append("このメールアドレスは既に使われています")
 
         if not password1:
-            errors.append("パスワードを入力してください")
-        if len(password1) < 8:
-            errors.append("パスワードは８文字以上に入力してください")
-        has_letter = any(char.isalpha() for char in password1)
-        has_number = any(char.isdigit() for char in password1)
-        if not (has_letter and has_number):
-            errors.append("パスワードは英字と数字の両方を含めてください")
+            errors.setdefault("password1", []).append("パスワードを入力してください")
+        else:
+            if len(password1) < 8:
+                errors.setdefault("password1", []).append("パスワードは８文字以上に入力してください")
+            has_letter = any(char.isalpha() for char in password1)
+            has_number = any(char.isdigit() for char in password1)
+            if not (has_letter and has_number):
+                errors.setdefault("password1", []).append("パスワードは英字と数字の両方を含めてください")
+
         if not password2:
-            errors.append("確認用パスワードを入力してください")
-        if password1 != password2:
-            errors.append("パスワードが一致しません")
+            errors.setdefault("password2", []).append("確認用パスワードを入力してください")
+        else:
+            if password1 and password1 != password2:
+                errors.setdefault("password2", []).append("パスワードが一致しません")
         
         #エラー時に入力内容保持する（パスワード以外）
         if errors:
