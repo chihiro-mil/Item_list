ed6a6b9144db6cca20d297276b6905d233525fcf
chihiro <ex14.mil30@gmail.com>
2026-01-18 21:49:12 +0900

    list_index view 完成

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import List
 
 
 @login_required
 def list_index(request):
+    if request.method == "POST":
+        title = request.POST.get("title", "").strip()
+        if title:
+            List.objects.create(
+                user=request.user,
+                title=title
+            )
+        return redirect("lists/list_index")
     lists = (List.objects.filter(user=request.user).order_by("-updated_at"))
     return render(request, "lists/index.html", {"lists": lists})
