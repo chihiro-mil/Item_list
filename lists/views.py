b5a764d8ff39b218212b963b58e8e33cca1f0652
chihiro <ex14.mil30@gmail.com>
2026-01-18 16:48:15 +0900

    lists views作成中

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import List
 
 
+@login_required
 def list_index(request):
-    return render(request, "lists/index.html")
+    lists = (List.objects.filter(user=request.user).order_by("-updated_at"))
+    return render(request, "lists/index.html", {"lists": lists})
